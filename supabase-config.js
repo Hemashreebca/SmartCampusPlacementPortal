efc78eb73ff50507d1ce88c82837cfbebeef69cb
Hemashreebca <hemumano24@gmail.com>
2026-09-19 11:35:07 +0530

    Connect student login and signup with Supabase

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pubksqxgouswmjetorfy.supabase.co/";
+const SUPABASE_KEY = "sb_publishable_KV_crssJBC3SZTNXnRfvSA_9sRSLCh7";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
